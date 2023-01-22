f69f33ce52903953fe350fb36ffc2c5ecc946e38
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2023-01-22 11:56:31 +0100

    Utworzenie repozytorium do kolokwium zaliczeniowego mpr

---
+++ src/main/java/kolokwiummpr/pjatk/PjatkApplication.java
@@ -0,0 +1,13 @@
+package kolokwiummpr.pjatk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjatkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjatkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kolokwiummpr/pjatk/PjatkApplicationTests.java
@@ -0,0 +1,13 @@
+package kolokwiummpr.pjatk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjatkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
